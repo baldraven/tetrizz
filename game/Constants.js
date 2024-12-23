80ef689fcbb2e86fbbc1804a7c572120c3eb6344
baldraven <elyaselaziz@gmail.com>
2024-12-23 18:49:09 +0100

    fix: orientation of J and L pieces

--- game/Constants.js
+++ game/Constants.js
@@ -2,8 +2,8 @@ export const SHAPES = {
   I: [[1, 1, 1, 1]],
   O: [[1, 1], [1, 1]],
   T: [[0, 1, 0], [1, 1, 1]],
-  L: [[1, 0], [1, 0], [1, 1]],
-  J: [[0, 1], [0, 1], [1, 1]],
+  L: [[0, 0, 1], [1, 1, 1]], // Changed from vertical to horizontal
+  J: [[1, 0, 0], [1, 1, 1]], // Changed from vertical to horizontal
   S: [[0, 1, 1], [1, 1, 0]],
   Z: [[1, 1, 0], [0, 1, 1]]
 };
